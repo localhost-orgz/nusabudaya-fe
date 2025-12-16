4bc66206bd2eba082ecc9b0098cc4d2f213da6ee
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2025-12-16 17:48:52 +0700

    fix(callback): move callback page

---
+++ src/utils/api.js
@@ -0,0 +1,31 @@
+export async function apiRequest(endpoint, { 
+   method = "GET", 
+   query = {}, 
+   body = null, 
+   headers: customHeaders = {}, 
+   isFormData = false 
+} = {}) {
+   const baseUrl = "https://nusabudaya-be.vercel.app";
+   const queryString = Object.keys(query).length ? "?" + new URLSearchParams(query).toString() : "";
+   const url = `${baseUrl}${endpoint}${queryString}`;
+
+   const headers = isFormData
+      ? customHeaders
+      : { "Content-Type": "application/json", ...customHeaders };
+
+   const options = { method, headers };
+   if (body) options.body = isFormData ? body : JSON.stringify(body);
+   try {
+      const res = await fetch(url, options);
+      const data = await res.json();
+
+      if (!res.ok) {
+         throw new Error(data.message || "Request failed");
+      }
+
+      return data;
+   } catch (err) {
+      console.error("API Request Error:", err);
+      throw err;
+   }
+}
